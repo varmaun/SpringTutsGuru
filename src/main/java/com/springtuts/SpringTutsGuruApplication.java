fb3534bf68b56eef7aad531a6c7136df753481b9
varmaun9 <varma.179@gmail.com>
2020-06-06 23:56:47 +0100

    spring tuts project

---
+++ src/main/java/com/springtuts/SpringTutsGuruApplication.java
@@ -0,0 +1,13 @@
+package com.springtuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutsGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutsGuruApplication.class, args);
+	}
+
+}
